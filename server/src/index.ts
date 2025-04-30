af34a75523e57a366c196c3e123ce330ed762c90
alex <alillolindo@gmail.com>
2025-04-30 12:41:33 +0200

    Ended

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,22 @@
-import { WebSocketServer } from 'ws'
+import { WebSocketServer, WebSocket } from 'ws'
 
 const wss = new WebSocketServer({ port: 8080 })
+const wsConnections: Set<WebSocket> = new Set()
 
 wss.on("connection", (ws) => {
     console.info("Client connected")
+    wsConnections.add(ws)
 
     ws.on("message", (msg) => {
         console.info("Message received:", msg.toString())
         ws.send(`Echo: ${msg}`)
+        for (const currentWs of wsConnections) {
+            currentWs.send(`${msg}`)
+        }
     })
 
     ws.on("close", () => {
+        wsConnections.delete(ws)
         console.info("Client disconnected")
     })
 })
