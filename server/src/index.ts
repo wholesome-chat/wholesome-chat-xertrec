a150dec8ba8375456d907cee755db5662200ba6f
alex <alillolindo@gmail.com>
2025-04-30 11:35:21 +0200

    Initial websocket commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,2 +1,18 @@
-const a: number = 1;
-console.info("hello world", a);
+import { WebSocketServer } from 'ws'
+
+const wss = new WebSocketServer({ port: 8080 })
+
+wss.on("connection", (ws) => {
+    console.info("Client connected")
+
+    ws.on("message", (msg) => {
+        console.info("Message received:", msg.toString())
+        ws.send(`Echo: ${msg}`)
+    })
+
+    ws.on("close", () => {
+        console.info("Client disconnected")
+    })
+})
+
+console.info("WebSocket server is running on ws:")
